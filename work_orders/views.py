b53a5cb9a9068faa8e38fd46693ae1f6b6f3dd4a
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-16 19:39:41 +0000

    Add work orders views and templates

--- work_orders/views.py
+++ work_orders/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import ScheduledWorkOrder, UnscheduledWorkOrder
+from .forms import ScheduledWorkOrderForm, UnScheduleWorkOrderForm
+from django.urls import reverse
+from equipment.models import Equipment
+from equipment.forms import EquipmentCreationForm
 # Create your views here.
+
+
+def scheduled_work_orders(request):
+    work_orders = ScheduledWorkOrder.objects.all()
+    context = {"title": "Scheduled Work Orders","work_orders":work_orders}
+    return render(request, "work_orders/scheduled_work_orders.html", context=context)
+
+
+def create_scheduled_work_order(request, asset_tag):
+    equipment = Equipment.objects.get(asset_tag=asset_tag)
+    equipment_form = EquipmentCreationForm(instance=equipment)
+    form = ScheduledWorkOrderForm()
+
+    if request.method == "POST":
+        form = ScheduledWorkOrderForm(request.POST)
+
+
+        if form.is_valid():
+            work_order = form.save(commit=False)
+            work_order.equipment = equipment
+            work_order.save()
+
+            return redirect(reverse("scheduled_work_orders"))
+
+        print(form.errors)
+
+    context = {
+        "work_order_form": form,
+        "equipment_form": equipment_form
+    }
+    return render(request, "work_orders/create_scheduled_work_order.html", context)
+
+
+def unscheduled_work_orders(request):
+    work_orders = UnscheduledWorkOrder.objects.all()
+    context = {"title": "Unscheduled Work Orders","work_orders":work_orders}
+    return render(request, "work_orders/unscheduled_work_orders.html", context=context)
+
+
+def create_unscheduled_work_order(request, asset_tag):
+    form = UnScheduleWorkOrderForm()
+    
+    equipment = Equipment.objects.get(asset_tag=asset_tag)
+    equipment_form = EquipmentCreationForm(instance=equipment)
+    
+
+    if request.method == "POST":
+        form = UnScheduleWorkOrderForm(request.POST)
+
+
+        if form.is_valid():
+            work_order = form.save(commit=False)
+            work_order.equipment = equipment
+            work_order.save()
+
+            return redirect(reverse("unscheduled_work_orders"))
+
+        print(form.errors)
+
+    context = {
+        "work_order_form": form,
+        "equipment_form": equipment_form
+    }
+
+    return render(request, "work_orders/create_unscheduled_work_order.html", context)
+
+

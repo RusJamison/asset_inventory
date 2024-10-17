e00c036b4b42b8eba856a1d3adbedf3b72aebd54
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-17 07:30:47 +0000

    readies for deployment

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c1b)ulc6*fre1)ih*deb4dihjdkybheimd%_fse7tg=y9535m('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition

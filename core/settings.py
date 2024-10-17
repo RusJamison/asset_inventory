c9a6db790cdd68e44acb7be328dfcf6368d86d4d
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-17 18:39:21 +0000

    Add static files and setting crispy forms

--- core/settings.py
+++ core/settings.py
@@ -47,12 +47,13 @@ INSTALLED_APPS = [
     'equipment',
     'work_orders',
     #'auth',
-    #'crispy_forms',
-    # 'crispy_bootstrap4'
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,8 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

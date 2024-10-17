60fcd80ae348f1881c7ff617358ae69d9af4f80f
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-17 19:12:38 +0000

    Register models to admin

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Equipment, Category, Manufacturer, HealthFacility, Department, ServiceProvider
 
 # Register your models here.
+
+admin.site.register(Equipment)
+admin.site.register(Category)
+admin.site.register(Manufacturer)
+admin.site.register(HealthFacility)
+admin.site.register(Department)
+admin.site.register(ServiceProvider)
+
+
--- work_orders/admin.py
+++ work_orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ScheduledWorkOrder, UnscheduledWorkOrder
 
 # Register your models here.
+admin.site.register(ScheduledWorkOrder)
+admin.site.register(UnscheduledWorkOrder)
+

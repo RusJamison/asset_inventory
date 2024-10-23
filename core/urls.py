6777a4f9e5e23b16a51f4e15ce2d7485754c42ff
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-23 07:53:36 +0000

    Add information on home page, fixed mobile first response and setup adding images to home page

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 def homepage(request):
     return render(request, 'home.html')
@@ -29,4 +31,4 @@ urlpatterns = [
     path('work_orders/', include('work_orders.urls')),
     path("accounts/", include("allauth.urls")),
    # path('auth/', include('auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

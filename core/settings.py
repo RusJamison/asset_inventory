44e79457584834ed8cafff09cb0fd6cda9cd50c7
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-10-23 07:59:00 +0000

    Set Debug to False

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c1b)ulc6*fre1)ih*deb4dihjdkybheimd%_fse7tg=y9535m('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-rusjamison-assetinvento-r0h57g8dv6d.ws.codeinstitute-ide.net',

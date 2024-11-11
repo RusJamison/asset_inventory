43b78806580109113c8c3a376d301f6045a5a30d
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-11-11 18:55:30 +0000

    Add PDF generation with reportlab

--- equipment/urls.py
+++ equipment/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("edit/<str:asset_tag>/", views.update_equipment, name="edit_equipment"),
     path("delete/<str:asset_tag>/", views.delete_equipment, name="delete_equipment"),
     path("search/", views.search_view, name="search_equipment"),
+    path('reports/equipment/', views.generate_equipment_pdf, name='equipment_pdf'),
     # path('equipment_list/', views.equipment_list, name='equipment_list'),
 ]
 

75867f0879217360d0b3cc8611ca4f67fa881f4e
Rusakaniko Jamison <jamisonr11@gmail.com>
2024-11-15 19:01:57 +0000

    Add labels for user manual and equipment image to update form

--- core/settings.py
+++ core/settings.py
@@ -23,12 +23,12 @@ import cloudinary.api
 from cloudinary.utils import cloudinary_url
 
 # Configuration       
-cloudinary.config( 
-    cloud_name = os.environ.get("CLOUDINARY_CLOUDNAME"), 
-    api_key = os.environ.get("CLOUDINARY_API_KEY"), 
-    api_secret = os.environ.get("CLOUDINARY_API_SECRET"), # Click 'View API Keys' above to copy your API secret
-    secure=True
-)
+#cloudinary.config( 
+    # cloud_name = os.environ.get("CLOUDINARY_CLOUDNAME"), 
+    # api_key = os.environ.get("CLOUDINARY_API_KEY"), 
+    # api_secret = os.environ.get("CLOUDINARY_API_SECRET"), # Click 'View API Keys' above to copy your API secret
+    # secure=True
+# )
 
 
 
